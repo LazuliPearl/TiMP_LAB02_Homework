5b0ef2806a25deb61e3162062d2e81c5dafb695a
LazuliPearl <kirillperepechkin@yandex.ru>
2022-03-18 06:21:43 -0400

    Fix codestyle

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,6 @@
 #include<iostream>
 
-using namespace std;
-
 int main() {
-	name = ${GITHIB_USERNAME};
-	cout << "Hello_World!" << @name << endl;
+	std::cout << "Hello_World!" << std::endl;
 	return 0;
 }
