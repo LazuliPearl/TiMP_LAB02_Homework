6b19a649d9a8ed490a17cad04baa005774ed6b98
LazuliPearl <89978544+LazuliPearl@users.noreply.github.com>
2022-03-21 10:41:53 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include<string>
 
 int main()
 {
-  string name;
-  cin >> name;
+  std::string name;
+  std::cin >> name;
   std::cout << "Hello_World!" << name << std::endl;
   return 0;
 }
