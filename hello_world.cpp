a612ad0f4655c0e228304d5b0ce995020efde9a7
LazuliPearl <89978544+LazuliPearl@users.noreply.github.com>
2022-03-21 10:40:55 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
+#include<string>
+
 
 int main() {
-	std::cout << "Hello_World!" << std::endl;
+	std::string name;
+	std::cin >> name;
+	std::cout << "Hello_World!" << name << std::endl;
 	return 0;
 }
 
